7ffdbc6af1a33a50a526e1efde31b7724de0e6fa
pengjiexu <wxdxfg@hotmail.com>
2018-06-05 22:25:34 +0800

    fix mutli coroutine print error

--- console.go
+++ console.go
@@ -5,28 +5,67 @@ import (
 	"time"
 )
 
+type log struct {
+	format string
+	prefix string
+	argv   []interface{}
+}
+
+var logChan chan *log
+
 func _log(format string, prefix string, a ...interface{}) (n int, err error) {
 	now := time.Now().Format("2006-01-02 15:04:05")
 	fmt.Printf("[%s] %s ", now, prefix)
-	return fmt.Printf(format + "\n", a...)
+	return fmt.Printf(format+"\n", a...)
 }
 
-func Ok(format string, a ...interface{}) (n int, err error) {
-	return _log(format, "\x1B[1;32m   [OK]\x1B[0m", a...)
+func init() {
+	logChan = make(chan *log)
+	go safeLoop()
 }
 
-func Log(format string, a ...interface{}) (n int, err error) {
-	return _log(format, "\x1B[1;34m  [LOG]\x1B[0m", a...)
+func safeLoop() {
+	log := <-logChan
+
+	_log(log.format, log.prefix, log.argv...)
 }
 
-func Err(format string, a ...interface{}) (n int, err error) {
-	return _log(format, "\x1B[1;31m  [ERR]\x1B[0m", a...)
+func Ok(format string, a ...interface{}) {
+	logChan <- &log{
+		format,
+		"\x1B[1;32m   [OK]\x1B[0m",
+		a,
+	}
 }
 
-func Warn(format string, a ...interface{}) (n int, err error) {
-	return _log(format, "\x1B[1;33m [WARN]\x1B[0m", a...)
+func Log(format string, a ...interface{}) {
+	logChan <- &log{
+		format,
+		"\x1B[1;34m  [LOG]\x1B[0m",
+		a,
+	}
 }
 
-func Debug(format string, a ...interface{}) (n int, err error) {
-	return _log(format, "\x1B[1;33m[DEBUG]\x1B[0m", a...)
+func Err(format string, a ...interface{}) {
+	logChan <- &log{
+		format,
+		"\x1B[1;31m  [ERR]\x1B[0m",
+		a,
+	}
+}
+
+func Warn(format string, a ...interface{}) {
+	logChan <- &log{
+		format,
+		"\x1B[1;33m [WARN]\x1B[0m",
+		a,
+	}
+}
+
+func Debug(format string, a ...interface{}) {
+	logChan <- &log{
+		format,
+		"\x1B[1;33m[DEBUG]\x1B[0m",
+		a,
+	}
 }
